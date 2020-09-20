97c8f0c096a31f9cb61777fc83399dfb845248c9
AmineDz <amine2014max@gmail.com>
2020-09-20 07:38:32 +0100

    Updates

---
+++ precache-manifest.e4ef1c9afa14cbaa971b40874d1687e5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d4e87f954b38574f5f3d1565fe0c57",
+    "url": "/projet/index.html"
+  },
+  {
+    "revision": "0d62aabf3e4425ff246e",
+    "url": "/projet/static/css/main.6fe44432.chunk.css"
+  },
+  {
+    "revision": "7c7d87dd92e91449bd45",
+    "url": "/projet/static/js/2.26377161.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/projet/static/js/2.26377161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d62aabf3e4425ff246e",
+    "url": "/projet/static/js/main.2c7e5603.chunk.js"
+  },
+  {
+    "revision": "c1fcec74f0c667d2f3c0",
+    "url": "/projet/static/js/runtime-main.71eea537.js"
+  },
+  {
+    "revision": "b08414c4763216285940be0b096bf25d",
+    "url": "/projet/static/media/Orkney-Light.b08414c4.otf"
+  },
+  {
+    "revision": "fb2c627341d675834a69791742b2962d",
+    "url": "/projet/static/media/Orkney-Medium.fb2c6273.otf"
+  },
+  {
+    "revision": "8b8f662ddb031372d328165bb2a4f6e5",
+    "url": "/projet/static/media/Orkney-Regular.8b8f662d.otf"
+  },
+  {
+    "revision": "ce9bdf49530925f52182ebe47da433b1",
+    "url": "/projet/static/media/svg1.ce9bdf49.svg"
+  },
+  {
+    "revision": "dcb0b4d7ab6ad4460a7e5b8da402afff",
+    "url": "/projet/static/media/svg2.dcb0b4d7.svg"
+  },
+  {
+    "revision": "f61f98408c60a79f2ce84c616f31c873",
+    "url": "/projet/static/media/svg3.f61f9840.svg"
+  },
+  {
+    "revision": "6ef0338f1d4457db709ff5079963a41c",
+    "url": "/projet/static/media/svg4.6ef0338f.svg"
+  },
+  {
+    "revision": "336337a8860a42ccd2b036458009f180",
+    "url": "/projet/static/media/svg5.336337a8.svg"
+  }
+]);
